930178a67fa93e04663839a5a3f03f166c6eb85e
tomas <tomasdc2016@gmail.com>
2022-10-25 18:06:36 -0300

    feat: Requisito1

--- desafio1.sql
+++ desafio1.sql
@@ -1,26 +1,167 @@
-DROP DATABASE IF EXISTS SpotifyClone;
+CREATE DATABASE IF NOT EXISTS SpotifyClone;
+USE SpotifyClone;
 
-  CREATE DATABASE IF NOT EXISTS SpotifyClone;
+CREATE TABLE planos(
+	plano_id INT PRIMARY KEY AUTO_INCREMENT,
+    plano_name VARCHAR(50) NOT NULL,
+    plano_valor INT NOT NULL
+);
 
-  CREATE TABLE SpotifyClone.tabela1(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
+CREATE TABLE artistas(
+	artista_id INT PRIMARY KEY AUTO_INCREMENT,
+    artista_name VARCHAR(50) NOT NULL
+);
 
-  CREATE TABLE SpotifyClone.tabela2(
-      coluna1 tipo restricoes,
-      coluna2 tipo restricoes,
-      colunaN tipo restricoes,
-  ) engine = InnoDB;
+CREATE TABLE albuns(
+	albun_id INT PRIMARY KEY AUTO_INCREMENT,
+    albun_name VARCHAR(100) NOT NULL,
+    artista_id INT NOT NULL,
+    FOREIGN KEY (artista_id) REFERENCES artistas(artista_id),
+    ano_lancamento INT NOT NULL
+);
 
-  INSERT INTO SpotifyClone.tabela1 (coluna1, coluna2)
+CREATE TABLE album_cancoes(
+	cancao_id INT PRIMARY KEY AUTO_INCREMENT,
+    album_id INT NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES albuns(albun_id),
+    cancao_name VARCHAR(100) NOT NULL,
+    duracao_segundos INT NOT NULL
+);
+
+CREATE TABLE usuario(
+	usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario_name VARCHAR(50) NOT NULL,
+    idade INT NOT NULL,
+    plano INT NOT NULL,
+    FOREIGN KEY (plano) REFERENCES planos(plano_id),
+    data_assinatura DATE
+);
+
+CREATE TABLE usuario_historico(
+	historico_id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario INT NOT NULL,
+    FOREIGN KEY (usuario) REFERENCES usuario(usuario_id),
+    cancao_id INT NOT NULL,
+    FOREIGN KEY (cancao_id) REFERENCES album_cancoes(cancao_id),
+    data_reproducao DATETIME
+);
+
+CREATE TABLE usuario_seguindo(
+	follow_id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario INT NOT NULL,
+    FOREIGN KEY (usuario) REFERENCES usuario(usuario_id),
+    artista_seguido INT NOT NULL,
+    FOREIGN KEY(artista_seguido) REFERENCES artistas(artista_id)
+);
+
+USE SpotifyClone;
+
+INSERT INTO planos VALUES(1, 'gratuito', 0.00);
+INSERT INTO planos VALUES(2, 'familiar', 7.99);
+INSERT INTO planos VALUES(3,'universitario', 5.99);
+INSERT INTO planos VALUES(4, 'pessoal', 6.99);
+
+INSERT INTO artistas  VALUES 
+(1,'Beyonce'),
+(2,'Queen'),
+(3,'Elis Regina'),
+(4,'Baco Exu do Blues'),
+(5,'Blind Guardian'),
+(6,'Nina Simone');
+
+INSERT INTO usuario (usuario_name, idade, plano, data_assinatura)
   VALUES
-    ('exemplo de dados 1', 'exemplo de dados A'),
-    ('exemplo de dados 2', 'exemplo de dados B'),
-    ('exemplo de dados 3', 'exemplo de dados C');
+    ('Barbara Liskov', 82, 1, '2019-10-20'),
+    ('Robert Cecil Martin', 58, 1, '2017-01-06'),
+    ('Ada Lovelace', 37, 2, '2017-12-30'),
+    ('Martin Fowler', 46, 2, '2017-01-17'),
+    ('Sandi Metz', 58, 2, '2018-04-29'),
+    ('Paulo Freire', 19, 3, '2018-02-14'),
+    ('Bell Hooks', 26, 3, '2018-01-05'),
+    ('Christopher Alexander', 85, 4, '2019-06-05'),
+    ('Judith Butler', 45, 4, '2020-05-13'),
+    ('Jorge Amado', 58, 4, '2017-02-17');
 
-  INSERT INTO SpotifyClone.tabela2 (coluna1, coluna2)
-  VALUES
-    ('exemplo de dados 1', 'exemplo de dados X'),
-    ('exemplo de dados 2', 'exemplo de dados Y');
+INSERT INTO albuns(albun_name,ano_lancamento,artista_id) VALUES
+('Renaissance', '2022', 1),
+('Jazz', '1978', 2),
+('Hot Space', '1982', 2),
+('Falso Brilhante', '1998', 3),
+('Vento de Maio', '2001', 3),
+('QVVJFA?', '2003', 4),
+('Somewhere Far Beyond', '2007', 5),
+('I Put A Spell On You', '2012', 6);
+
+INSERT INTO albuns(albun_name,ano_lancamento,artista_id) VALUES
+('Renaissance', '2022', 1),
+('Jazz', '1978', 2),
+('Hot Space', '1982', 2),
+('Falso Brilhante', '1998', 3),
+('Vento de Maio', '2001', 3),
+('QVVJFA?', '2003', 4),
+('Somewhere Far Beyond', '2007', 5),
+('I Put A Spell On You', '2012', 6);
+
+INSERT INTO album_cancoes(cancao_name,duracao_segundos,album_id) VALUES
+('BREAK MY SOUL', 279, 1),
+('VIRGO\’S GROOVE', 369, 1),
+('ALIEN SUPERSTAR', 116, 1),
+('Don\’t Stop Me Now', 203, 2),
+('Under Pressure', 152, 3),
+('Como Nossos Pais', 105, 4),
+('O Medo de Amar é o Medo de Ser Livre', 207, 5),
+('Samba em Paris', 267, 6),
+('The Bard’s Song', 244, 7),
+('Feeling Good', 100, 8);
+
+INSERT INTO usuario_historico(data_reproducao,usuario,cancao_id) VALUES
+('2022-02-28 10:45:55', 1, 8),
+('2020-05-02 05:30:35', 1, 2),
+('2020-03-06 11:22:33', 1, 10),
+('2022-08-05 08:05:17', 2, 10),
+('2020-01-02 07:40:33', 2, 7),
+('2020-11-13 16:55:13', 3, 10),
+('2020-12-05 18:38:30', 3, 2),
+('2021-08-15 17:10:10', 4, 8),
+('2022-01-09 01:44:33', 5, 8),
+('2020-08-06 15:23:43', 5, 5),
+('2017-01-24 00:31:17', 6, 7),
+('2017-10-12 12:35:20', 6, 1),
+('2011-12-15 22:30:49', 7, 4),
+('2012-03-17 14:56:41', 8, 4),
+('2022-02-24 21:14:22', 9, 9),
+('2015-12-13 08:30:22', 10, 3);
+
+INSERT INTO usuario_historico(data_reproducao,usuario, cancao_id) VALUES
+('2022-02-28 10:45:55', 1, 8),
+('2020-05-02 05:30:35', 1, 2),
+('2020-03-06 11:22:33', 1, 10),
+('2022-08-05 08:05:17', 2, 10),
+('2020-01-02 07:40:33', 2, 7),
+('2020-11-13 16:55:13', 3, 10),
+('2020-12-05 18:38:30', 3, 2),
+('2021-08-15 17:10:10', 4, 8),
+('2022-01-09 01:44:33', 5, 8),
+('2020-08-06 15:23:43', 5, 5),
+('2017-01-24 00:31:17', 6, 7),
+('2017-10-12 12:35:20', 6, 1),
+('2011-12-15 22:30:49', 7, 4),
+('2012-03-17 14:56:41', 8, 4),
+('2022-02-24 21:14:22', 9, 9),
+('2015-12-13 08:30:22', 10, 3);
+
+INSERT INTO usuario_seguindo(usuario,artista_seguido) VALUES
+(1, 1),
+    (1, 2),
+    (1, 3),
+    (2, 1),
+    (2, 3),
+    (3, 2),
+    (4, 4),
+    (5, 5),
+    (5, 6),
+    (6, 1),
+    (6, 6),
+    (7, 6),
+    (9, 3),
+    (10, 2);
